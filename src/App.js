0e71d7ceb818102b1969fe4ed01c936483f04401
Margit Tennosaar <margit.tennosaar@bc.fi>
2021-11-15 13:45:40 +0200

    added random number functionality

--- src/App.js
+++ src/App.js
@@ -3,22 +3,68 @@ import "./App.css";
 import Circle from "./Circle";
 import { circles } from "./circles";
 
+const getRndInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
 class App extends Component {
-  state = {};
+  state = {
+    score: 0,
+    current: 0,
+  };
+
+  timer = undefined;
+  pace = 1500;
+
+  clickHandler = () => {
+    this.setState({
+      score: this.state.score + 10,
+    });
+  };
+
+  nextCircle = () => {
+    let nextActive;
+
+    do {
+      nextActive = getRndInteger(1, 4);
+    } while (nextActive === this.state.current);
+
+    this.setState({
+      current: nextActive,
+    });
+
+    this.pace *= 0.95;
+    this.timer = setTimeout(this.nextCircle, this.pace);
+
+    console.log("active circle is ", this.state.current);
+  };
+
+  startHandler = () => {
+    this.nextCircle();
+  };
+
+  stopHandler = () => {
+    clearTimeout(this.timer);
+  };
 
   render() {
     return (
       <div>
         <h1>SpeedGame</h1>
-        <p>Your score:</p>
+        <p>Your score: {this.state.score}</p>
         <div className="circles">
           {circles.map((c) => (
-            <Circle key={c.id} color={c.color} id={c.id} />
+            <Circle
+              key={c.id}
+              color={c.color}
+              id={c.id}
+              click={this.clickHandler}
+            />
           ))}
         </div>
         <div>
-          <button>Start</button>
-          <button>Stop</button>
+          <button onClick={this.startHandler}>Start</button>
+          <button onClick={this.stopHandler}>Stop</button>
         </div>
       </div>
     );
--- src/Circle.js
+++ src/Circle.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Circle = (props) => {
   return (
-    <div style={{ backgroundColor: props.color }} className="circle">
+    <div
+      style={{ backgroundColor: props.color }}
+      className="circle"
+      onClick={props.click}
+    >
       <p> {props.id}</p>
     </div>
   );
